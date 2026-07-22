4cf90ab2f7017b2213c457c5acd2c9f2f2896371
Vorsclica <vorsclica@gmail.com>
2026-07-22 22:27:58 +0300

    Created post 03-input set 2

---
+++ games/03-input/content/data-002/info.js
@@ -0,0 +1,13 @@
+export default {
+  id: "data-002",
+
+  title: "Задача дня:",
+
+  description: "Логічна задача з відкритою відповіддю.",
+
+  version: "1.0",
+
+  author: "Logic Games",
+
+  created: "2026-07-22",
+};
---
+++ games/03-input/content/data-002/task.js
@@ -0,0 +1,9 @@
+export default {
+  question: `Знайдіть відстань між котиком та песиком`,
+
+  image: "06.png",
+
+  answers: ["8", "вісім"],
+
+  explanation: ``,
+};
