a715b5c995adea2700dcf2b2d26b44ad69404f09
Vorsclica <vorsclica@gmail.com>
2026-08-15 13:31:17 +0300

    post 5

---
+++ games/03-input/content/data-003/info.js
@@ -0,0 +1,13 @@
+export default {
+  id: "data-003",
+
+  title: "Знайдіть суму:",
+
+  description: "Логічна задача з відкритою відповіддю.",
+
+  version: "1.0",
+
+  author: "Logic Games",
+
+  created: "2026-08-15",
+};
---
+++ games/03-input/content/data-003/task.js
@@ -0,0 +1,9 @@
+export default {
+  question: "Знайдіть суму чисел",
+
+  image: "fr001.jpg",
+
+  answers: ["15"],
+
+  explanation: "",
+};
