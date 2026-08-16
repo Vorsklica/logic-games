52f8926ac330b289e660ac4495b4d9ad071d5552
Vorsclica <vorsclica@gmail.com>
2026-08-16 19:46:07 +0300

    data-002 restored

---
+++ games/03-input/content/data-002/task.js
@@ -0,0 +1,8 @@
+export default {
+  title: "Задача дня",
+  image: "06.png",
+  question: `Знайдіть відстань між котиком та песиком`,
+  answers: ["8", "вісім"],
+  type: "number",
+  explanation: ``,
+};
