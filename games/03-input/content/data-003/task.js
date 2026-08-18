9578dac85312cfdf5bfc3d803ce5dc854244617e
Vorsclica <vorsclica@gmail.com>
2026-08-18 19:45:42 +0300

    change extention image

--- games/03-input/content/data-003/task.js
+++ games/03-input/content/data-003/task.js
@@ -2,7 +2,7 @@ export default {
   title: "Геометрична головоломка",
   image: "mm.jpg",
   question: `Знайдіть невідому відстань`,
-  answers: [20],
+  answers: ["20"],
   type: "number",
   explanation: ``,
 };
